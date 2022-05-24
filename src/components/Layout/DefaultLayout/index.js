3e030926891ba44edc22bfd1f48cde0d565823ae
tanhoang0710 <tanhoang0710@gmail.com>
2022-05-24 20:47:12 +0700

    63. Cấu hình Router/Layout cho dự án

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+export default function DefaultLayout({ children }) {
+	return (
+		<div>
+			<Header />
+			<div className="container">
+				<Sidebar />
+				<div className="content">{children}</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/routes/index.js
@@ -0,0 +1,38 @@
+// Layouts
+import { HeaderOnly } from "~/components/Layout";
+
+import Following from "~/components/pages/Following";
+import Home from "~/components/pages/Home";
+import Profile from "~/components/pages/Profile";
+import Upload from "~/components/pages/Upload";
+import Search from "~/components/pages/Search";
+
+// Public routes
+const publicRoutes = [
+	{
+		path: "/",
+		component: Home,
+	},
+	{
+		path: "/following",
+		component: Following,
+	},
+	{
+		path: "/profile",
+		component: Profile,
+	},
+	{
+		path: "/upload",
+		component: Upload,
+		layout: HeaderOnly,
+	},
+	{
+		path: "/search",
+		component: Search,
+		layout: null,
+	},
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
