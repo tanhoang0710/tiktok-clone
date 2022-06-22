4aea337a1155b69e0a830b76155f4ca5a76e07ea
tanhoang0710 <tanhoang0710@gmail.com>
2022-06-22 22:16:35 +0700

    112. Tìm hiểu thư viện PropTypes

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classNames from "classnames/bind";
 import styles from "./AccountItem.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -31,3 +32,7 @@ export default function AccountItem({ data }) {
 		</Link>
 	);
 }
+
+AccountItem.propTypes = {
+	data: PropTypes.object,
+};
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classNames from "classnames/bind";
 import styles from "./Button.module.scss";
 import { Link } from "react-router-dom";
@@ -64,3 +65,7 @@ export default function Button({
 		</Comp>
 	);
 }
+
+Button.propTypes = {
+	children: PropTypes.node.isRequired,
+};
